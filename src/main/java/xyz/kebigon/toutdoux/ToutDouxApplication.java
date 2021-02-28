3f21142413a8cc0e93c4f0345b15c093f86bdf2f
Kebigon <git@kebigon.xyz>
2021-02-28 11:59:57 +0900

    Create Spring Boot project

---
+++ src/main/java/xyz/kebigon/toutdoux/ToutDouxApplication.java
@@ -0,0 +1,13 @@
+package xyz.kebigon.toutdoux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToutDouxApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ToutDouxApplication.class, args);
+	}
+}
---
+++ src/test/java/xyz/kebigon/toutdoux/ToutdouxApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.kebigon.toutdoux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToutdouxApplicationTests
+{
+	@Test
+	void contextLoads()
+	{
+	}
+}
